7c73a5405b5093e659a52ce955e69bf3cfee6269
Luis Mendes <machadoluismendes@gmail.com>
2021-12-13 16:40:41 -0300

    config
    netlify config
    docker config

--- netlify.sh
+++ netlify.sh
@@ -5,5 +5,6 @@ fluentd-ui start --daemonize
 gem install bundler --version 1.17.3
 bundle install --path vendor/bundle
 
-docker build -t fluent/fluentd-ui:1.0.0 .
-docker run --net=host fluent/fluentd-ui:1.0.0
+docker build -t poligonosapp/fluentd-ui:1.0.0 .
+docker run --net=host poligonosapp/fluentd-ui:1.0.0
+docker push poligonosapp/fluentd-ui:1.0.0
